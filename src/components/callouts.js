bff29d4c8e475d5fbf2ce08bc18a708f8ab58b05
Jay Chan <jaychan688@gmail.com>
2021-07-15 17:55:21 +0800

    Basic setting.

---
+++ src/components/callouts.js
@@ -0,0 +1,1 @@
+import './callouts.css'
---
+++ webpack.dev.js
@@ -0,0 +1,124 @@
+const path = require('path')
+// Depend on npm scripts we define: npm_lifecycle_event:
+const currentTask = process.env.npm_lifecycle_event
+// Remove/clean your build folder(s).
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// Extracts CSS into separate files.
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+//  Plugin that simplifies creation of HTML files to serve your bundles
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+// Replacement for native fs
+const fse = require('fs-extra')
+
+const cssConfig = {
+	test: /\.css$/i,
+	use: ['css-loader', 'postcss-loader'],
+}
+
+const pages = fse
+	.readdirSync('./src/components')
+	.filter(file => {
+		return file.endsWith('.html')
+	})
+	.map(page => {
+		return new HtmlWebpackPlugin({
+			filename: page,
+			template: `./src/components/${page}`,
+			// entry 要搭配  chunks
+			chunks: [page.slice(0, page.indexOf('.'))],
+		})
+	})
+
+const config = {
+	entry: {
+		link: './src/components/link.js',
+		badge: './src/components/badge.js',
+		blocks: './src/components/blocks.js',
+		button: './src/components/button.js',
+		callouts: './src/components/callouts.js',
+	},
+	module: {
+		rules: [
+			cssConfig,
+			{
+				test: /\.js$/,
+				exclude: /(node_modules)/,
+				use: ['babel-loader'],
+			},
+			// Webpack build in asset modules - is type not use loader
+			// For Images
+			{
+				test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
+				type: 'asset/resource',
+			},
+			// For Fonts and SVGs
+			{
+				test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
+				type: 'asset/inline',
+			},
+		],
+	},
+	plugins: pages,
+}
+
+if (currentTask === 'dev') {
+	cssConfig.use.unshift('style-loader')
+
+	config.output = {
+		// multi-page seeting: [name] to output different js to match html.
+		filename: '[name].js',
+		// output the bundle.js to the src folder,
+		// side by side by the index.html, using absolute path.
+		path: path.resolve(__dirname, 'src'),
+	}
+
+	config.devServer = {
+		// Middleware: modify html file and auto reload, full page reload
+		before: (app, server) => {
+			server._watch('./src/**/*.html')
+		},
+		contentBase: path.join(__dirname, 'src'),
+		hot: true,
+		open: true,
+		port: 3000,
+		historyApiFallback: true,
+		// Let server to be accessible externally, allow mobile test
+		// Modify package.json, scripts -> "start": "webpack serve --open  --host localhost"
+		host: '0.0.0.0',
+	}
+
+	config.mode = 'development'
+	config.devtool = 'source-map'
+}
+
+// Copy images folder
+class RunAfterCompile {
+	apply(compiler) {
+		compiler.hooks.done.tap('Copy images', () => {
+			fse.copySync('./src/images', './dist/images')
+		})
+	}
+}
+
+if (currentTask === 'build') {
+	cssConfig.use.unshift(MiniCssExtractPlugin.loader)
+
+	config.output = {
+		filename: '[name].[chunkhash].js',
+		chunkFilename: '[name].[chunkhash].js',
+		path: path.resolve(__dirname, 'dist'),
+	}
+
+	config.mode = 'production'
+	config.optimization = {
+		splitChunks: { chunks: 'all' },
+	}
+
+	config.plugins.push(
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin({ filename: 'style.[chunkhash].css' }),
+		new RunAfterCompile()
+	)
+}
+
+module.exports = config
